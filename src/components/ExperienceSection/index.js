9dd176ae9286eef2282dffa4dbfb7f24f8fa5254
Amish Patel <amishpatel94@hotmail.com>
2020-08-22 14:15:19 -0400

    Add styling to the experience timeline

--- src/components/ExperienceSection/index.js
+++ src/components/ExperienceSection/index.js
@@ -3,39 +3,51 @@ import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timel
 import WorkIcon from '@material-ui/icons/Work'
 import SchoolIcon from '@material-ui/icons/School'
 import StarIcon from '@material-ui/icons/Star'
+import { Typography } from '@material-ui/core'
 import 'react-vertical-timeline-component/style.min.css';
 import config from '../../../config';
+import './index.scss';
 
 export default function() {
+  const renderSkillBadges = (skills) => {
+    return skills.map((skill) => (
+      <Typography variant="body2" className="skill-badge">
+        {skill}
+      </Typography>
+    ))
+  }
+
   const renderTimelineElement = (experience) => {
     if(experience.type === 'work') {
       return (
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          contentStyle={{ background: '#86b2d6', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid #86b2d6' }}
           date={experience.date}
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          iconStyle={{ background: '#86b2d6', color: '#fff' }}
           icon={<WorkIcon />}
         >
           <h3 className="vertical-timeline-element-title">{experience.title}</h3>
-          <h4 className="vertical-timeline-element-subtitle">{experience.location}</h4>
-          <p>{experience.description}</p>
+          <h4 className="vertical-timeline-element-subtitle">{experience.organization} - {experience.location}</h4>
+          <Typography variant="subtitle1">{experience.description}</Typography>
+          <div className="skills-container">{ renderSkillBadges(experience.skills) }</div>
         </VerticalTimelineElement>
       )
     } else {
       return (
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          contentStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-          contentArrowStyle={{ borderRight: '7px solid  rgb(233, 30, 99)' }}
+          contentStyle={{ background: '#f9b9a3', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid #f9b9a3' }}
           date={experience.date}
-          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+          iconStyle={{ background: '#f9b9a3', color: '#fff' }}
           icon={<SchoolIcon />}
         >
           <h3 className="vertical-timeline-element-title">{experience.title}</h3>
-          <h4 className="vertical-timeline-element-subtitle">{experience.location}</h4>
-          <p>{experience.description}</p>
+      <h4 className="vertical-timeline-element-subtitle">{experience.organization} - {experience.location}</h4>
+          <Typography variant="subtitle1">{experience.description}</Typography>
+          <div className="skills-container">{ renderSkillBadges(experience.skills) }</div>
         </VerticalTimelineElement>
       )
     }
@@ -51,7 +63,7 @@ export default function() {
           config.experiences.map((experience) => (renderTimelineElement(experience)))
         }
         <VerticalTimelineElement
-          iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+          iconStyle={{ background: '#3bff80', color: '#fff' }}
           icon={<StarIcon />}
         />
       </VerticalTimeline>
